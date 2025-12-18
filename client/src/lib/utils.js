adc198f3b6bc0f1b0b60b093b14cce0f0b8dc843
Vikashh78 <srmavikash10@gmail.com>
2025-12-18 20:51:03 +0530

    Refactor RightSidebar and ChatContainer components for improved user experience

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -1,5 +1,5 @@
 export function formatMessageTime(date) {
-    // console.log("Message date:", date);
+
     return new Date(date).toLocaleTimeString("en-US", {
         hour: "2-digit",
         minute: "2-digit",
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,17 +1,18 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
 import RightSidebar from '../components/RightSidebar'
+import { ChatContext } from '../../context/ChatContext'
 
 const Homepage = () => {
-    const [selectedUser, setSelectedUser] = useState(false)
+    const { selectedUser, setSelectedUser } = useContext(ChatContext)
 
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
         <div className={`backdrop-blur-xl border-2 bor-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-1 relative ${selectedUser? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]': 'md:grid-cols-2'}`}>
             <Sidebar />
             <ChatContainer />
-            <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+            <RightSidebar />
         </div> 
     </div>
   )
